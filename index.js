d8c60dd2c3fa1bb99ded55ce29659dd3b3ad7803
Tuomo Helo <tuomo.helo@turkuamk.fi>
2023-04-23 20:35:24 +0300

    Change message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Brave World!</h1>')
+  res.send('<h1>Hello Brave World 1!</h1>')
 })
 
 const PORT = 8080
